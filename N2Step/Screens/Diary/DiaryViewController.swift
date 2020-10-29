903b69b27f893047795df5fc09258342f38722f0
Marco Zimmermann <zimmermann@ubique.ch>
2020-10-29 22:42:58 +0100

    N2S: adds basic face ID

--- N2Step/Screens/Diary/DiaryViewController.swift
+++ N2Step/Screens/Diary/DiaryViewController.swift
@@ -10,6 +10,7 @@
  */
 
 import Foundation
+import LocalAuthentication
 
 class DiaryViewController : BaseViewController
 {
@@ -23,5 +24,57 @@ class DiaryViewController : BaseViewController
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        authenticate()
+    }
+
+    // MARK: - Authentication
+
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)
+        {
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "face_id_reason_text".ub_localized) { success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self.showDiary()
+                    } else {
+                        if let err = authenticationError
+                        {
+                            self.handleError(err)
+                        }
+                        else
+                        {
+                            self.showDiary()
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            // no biometrics
+            self.showDiary()
+        }
+    }
+
+    private func showDiary()
+    {
+        // TODO: show diary
+
+    }
+
+    private func handleError(_ error : Error)
+    {
+        // TODO: handle error
+        let alert = UIAlertController(title: "Error", message: "Error", preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
     }
 }
