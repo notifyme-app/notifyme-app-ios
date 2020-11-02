1a9bc970d28463190d2971827ccdb72a782ec2f4
Marco Zimmermann <zimmermann@ubique.ch>
2020-11-02 09:21:20 +0100

    N2STEP: adds possibility to set reminders

--- N2Step/Logic/ReminderManager/ReminderManager.swift
+++ N2Step/Logic/ReminderManager/ReminderManager.swift
@@ -10,3 +10,64 @@
  */
 
 import Foundation
+
+class ReminderManager : NSObject
+{
+    private let notificationCategory = "reminder"
+
+    // MARK: - Shared instance
+
+    public static let shared = ReminderManager()
+
+    // MARK: - Public API
+
+    public func scheduleReminder(for id: Int, in timeInterval: TimeInterval)
+    {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                self.scheduleNotification(for: id, in: timeInterval)
+            } else {
+                // TODO: Problem.
+            }
+        }
+    }
+
+    public func removeAllReminder()
+    {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.removeAllDeliveredNotifications()
+    }
+
+    // MARK: - Implementation
+
+    private func scheduleNotification(for id: Int, in timeInterval: TimeInterval)
+    {
+        let notificationCenter = UNUserNotificationCenter.current()
+        let notification = UNMutableNotificationContent()
+
+        notification.title = "checkout_reminder_title".ub_localized
+        notification.body = "checkout_reminder_text".ub_localized
+        notification.categoryIdentifier = self.notificationCategory
+        notification.sound = UNNotificationSound.default
+
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+        let notificationRequest = UNNotificationRequest(identifier: UUID().uuidString, content: notification, trigger: trigger)
+        notificationCenter.add(notificationRequest)
+    }
+}
+
+extension ReminderManager : UNUserNotificationCenterDelegate
+{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+    {
+        completionHandler([.alert, .badge, .sound])
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void)
+    {
+        completionHandler()
+    }
+}
